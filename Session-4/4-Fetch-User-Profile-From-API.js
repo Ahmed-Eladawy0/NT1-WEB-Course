6d630b29bc80354ea5846e957e2f0e877ae64872
Ahmed Elaawy <ahmmedstudy2004@gmail.com>
2026-07-20 15:31:19 +0300

    Add Session 4 tasks

--- Session-4/4-Fetch-User-Profile-From-API.js
+++ Session-4/4-Fetch-User-Profile-From-API.js
@@ -1,6 +1,6 @@
 async function getUserProfile(id) {
     try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${1}`);
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
         if (!response.ok) throw new Error("User not found");
         const user = await response.json();
         console.log(`Name: ${user.name}`);
