714d6c3ac8e4273914054742a82e9aa65b0523bc
Ahmed Elaawy <ahmmedstudy2004@gmail.com>
2026-08-05 15:52:52 +0300

    Session 8 task

--- Session-11/Task/Backend/index.js
+++ Session-11/Task/Backend/index.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dbConnect = require("./config/db-connect");
 const courseRouter = require("./routes/course-routes");
 const authRouter = require("./routes/auth-routes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+dbConnect();
 app.use("/api/v1/courses", courseRouter);
 app.use("/api/v1/auth", authRouter);
-
-app.listen(5000, () => {
-  console.log("Server listening on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
--- Session-11/Task/Backend/routes/course-routes.js
+++ Session-11/Task/Backend/routes/course-routes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllCourses,
+  getCourseById,
   createCourse,
   updateCourse,
   deleteCourse,
 } = require("../controllers/course-controllers");
 
 router.route("/").get(getAllCourses).post(createCourse);
-
-router.route("/:id").put(updateCourse).delete(deleteCourse);
+router.route("/:id").get(getCourseById).patch(updateCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
